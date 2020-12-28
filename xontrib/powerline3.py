a30f79597ca0fa5be17bdf6fe46d2e3824495c21
Noortheen Raja <jnoortheen@gmail.com>
2020-12-28 23:46:09 +0530

    feat: move modules out of xontrib

---
+++ xontrib/powerline3.py
@@ -0,0 +1,2 @@
+import xontrib_powerline3.processor
+import xontrib_powerline3.fields  # register extra fields
--- xontrib/powerline3/__init__.py
+++
@@ -1,2 +0,0 @@
-from .processor import process_prompt_tokens
-from . import fields  # register extra fields
